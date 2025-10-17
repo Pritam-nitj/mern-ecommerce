90aa5456246f903cb75df682116d0a07c18baf2c
Pritam <pritamk6284987295@gmail.com>
2025-10-17 21:12:05 +0530

    cloudinary-error-fix

--- server/controllers/common/feature-controller.js
+++ server/controllers/common/feature-controller.js
@@ -1,29 +1,73 @@
 const Feature = require("../../models/Feature");
+const cloudinary = require('../../helpers/cloudinary');
+
 
 const addFeatureImage = async (req, res) => {
   try {
-    const { image } = req.body;
+    // 1. Check if a file was uploaded
+    if (!req.file) {
+      return res.status(400).json({
+        success: false,
+        message: "No image file provided.",
+      });
+    }
 
-    console.log(image, "image");
+    // 2. Convert buffer to a data URI for Cloudinary
+    // This makes it easy for Cloudinary to process the image from memory.
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
 
-    const featureImages = new Feature({
-      image,
+    // 3. Upload the image to Cloudinary
+    const uploadResult = await cloudinary.uploader.upload(dataURI, {
+      folder: "feature_images", // Optional: organize uploads in a specific folder
     });
 
-    await featureImages.save();
+    // 4. Create a new document in your database with the Cloudinary URL
+    const feature = new Feature({
+      image: uploadResult.secure_url, // Use the secure HTTPS URL
+    });
 
+    // 5. Save the document to the database
+    await feature.save();
+
+    // 6. Send success response
     res.status(201).json({
       success: true,
-      data: featureImages,
+      message: "Feature image uploaded successfully!",
+      data: feature,
     });
+
   } catch (e) {
-    console.log(e);
+    console.error("Error uploading feature image:", e);
     res.status(500).json({
       success: false,
-      message: "Some error occured!",
+      message: "Some error occurred!",
     });
   }
 };
+
+// const addFeatureImage = async (req, res) => {
+//   try {
+//     const { image } = req.body;
+
+//     const featureImages = new Feature({
+//       image,
+//     });
+
+//     await featureImages.save();
+
+//     res.status(201).json({
+//       success: true,
+//       data: featureImages,
+//     });
+//   } catch (e) {
+//     console.log(e);
+//     res.status(500).json({
+//       success: false,
+//       message: "Some error occured!",
+//     });
+//   }
+// };
 
 const getFeatureImages = async (req, res) => {
   try {
